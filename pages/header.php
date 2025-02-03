6fe4596d44d3169afebd7d9d112b5e06a066dc3b
Tanguy Carpentier <carpentiertanguy@gmail.com>
2025-02-03 15:18:51 +0100

    mise en place

---
+++ pages/header.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+        <ul>
+            <li><a href="#about">À propos</a></li>
+            <li><a href="#education">Parcours scolaire</a></li>
+            <li><a href="#experience">Expériences</a></li>
+            <li><a href="#hobbies">Hobbies</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+</nav>
